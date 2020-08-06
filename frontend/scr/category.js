081936c0d9cd4c8b57be6e2aa889f388a1afd276
hnnav <henna.v@icloud.com>
2020-08-06 11:04:26 +0300

    category and destination classes

---
+++ frontend/scr/category.js
@@ -0,0 +1,8 @@
+class Category{
+    constructor(name, image_scr){
+        this.name = name;
+        this.image_scr = image_scr;
+    }
+
+    // instace method that renders object to dom
+}
---
+++ frontend/scr/destination.js
@@ -0,0 +1,9 @@
+class Destination{
+    constructor(name, decription, image_scr){
+        this.name = name;
+        this.image_scr = image_scr;
+        this.description = description;
+    }
+
+    // instace method that renders object to dom
+}
