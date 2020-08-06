d7e447d4833c7ec1821c87adb5919cac46fb3ecf
hnnav <henna.v@icloud.com>
2020-08-06 19:37:45 +0300

    destination form submission

--- frontend/index.js
+++ frontend/index.js
@@ -38,7 +38,34 @@ function createForm(){
         <input type="submit" value="Create Destination">
     </form>
     `
-    destinationForm.addEventListener("submit", () =>{
-        debugger;
+    destinationForm.addEventListener("submit", destinationFormSubmission)
+}
+
+function destinationFormSubmission(){
+    event.preventDefault();
+    let name = document.getElementById("name").value
+    let description = document.getElementById("description").value
+    let image_scr = document.getElementById("image_scr").value
+
+    let destination = {
+        name: name,
+        description: description,
+        image_scr: image_scr,
+    }
+
+    fetch(`${BASE_URL}/categories`{
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(destination)
+    })
+    .then(resp => resp.json())
+    .then(destination => {
+        let d = new Destination(destination.id, destination.name, destination.description, destination.image_scr)
+        d.renderDestination();
     })
 }
+
+
