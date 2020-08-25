ae719b4d886cb3056a062c2cf4909c85304e327b
hnnav <henna.v@icloud.com>
2020-08-25 20:35:10 +0300

    new destination for submit

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     fetchDestination();
+    createDestinationForm();
 });
 
 const BASE_URL = "http://localhost:3000"
@@ -12,29 +13,94 @@ function fetchDestination() {
  .then(resp => resp.json())
  .then(destinations => {
      for (const destination of destinations){
-         let c = new Destination(destination.id, destination.name, destination.image_scr, destination.description, destination.category)
-         c.renderDestination();
+         let d = new Destination(destination.id, destination.name, destination.image_scr, destination.description, destination.category)
+         d.renderDestination();
      }
      })
 }
-// 
-// // NEW CATEGORY FORM
-// 
-// function createCategoryForm(){
-//     let categoryForm = document.getElementById("new-category")
-// 
-//     categoryForm.innerHTML +=
-//     `
-//     <h3> Create a new category: </h3>
-//     <form>
-//         <label> Name category: </label> <input type="text" id="name"><br>
-//         <label> Add an image: </label> <input type="text" id="image_scr"><br>
-//         <input type="submit" value="Create Category">
-//     </form>
-//     `
-// 
-//     categoryForm.addEventListener("submit", categoryFormSubmission)
-// }
+
+// NEW DESTINATION FORM
+
+function createDestinationForm() {
+    let destinationForm = document.getElementById("new-destination")
+
+    destinationForm.innerHTML +=
+     `
+     <h3> Create a new destination: </h3>
+     <form>
+        <input type="text" id="name" placeholder="Name"><br>
+
+        <textarea name="description" id="description" rows="7" cols="30" placeholder="Description"></textarea><br>
+
+        <input type="text" id="image_scr" placeholder="Image link"><br><br>
+
+        <label> Choose a category </label><br>
+        <select id="category-select" name="categories"> 
+            <option value="1"> City breaks </option>
+            <option value="2"> Beach Destinations </option>
+            <option value="3"> Family holidays </option>
+        </select><br><br>
+
+         <input type="submit" value="Create Destination" >
+     </form>
+     `
+
+    destinationForm.addEventListener("submit", (e) =>
+    destinationFormHandler(e))
+}
+
+// HANDLE FORM DATA
+
+function destinationFormHandler(){
+    event.preventDefault()
+
+    let name = document.getElementById("name").value
+    let description = document.getElementById("description").value
+    let image_scr = document.getElementById("image_scr").value
+    let categoryId = parseInt(document.getElementById("category-select").value) // returns id
+    destinationFormSubmission(name, description, image_scr, categoryId)
+}
+
+// SUBMIT FORM DATA
+
+function destinationFormSubmission(name, description, image_scr, category_id){
+    fetch(`${BASE_URL}/destinations`, {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            description: description,
+            image_scr: image_scr,
+            category_id: category_id
+        })
+    })
+    .then(resp => resp.json())
+    .then(destination => {
+        let d = new Destination(destination.id, destination.name, destination.image_scr, destination.description, destination.category)
+        d.renderDestination();
+    })
+}
+
+
+    // 
+    // 
+    //     fetch(`${BASE_URL}/categories`, {
+    //         method: "POST",
+    //         headers: {
+    //             'Accept': 'application/json',
+    //             'Content-type': 'application/json'
+    //         },
+    //         body: JSON.stringify(destination)
+    //     })
+    //     .then(resp => resp.json())
+    //     .then(destination => {
+    //         let d = new Destination(destination.id, destination.name, destination.description, destination.image_scr)
+    //         d.renderDestination();
+    //     })
+    // }
 // 
 // // SUBMIT CATEGORY FORM
 // 
@@ -64,22 +130,7 @@ function fetchDestination() {
 //     })
 // }
 // 
-// // create - create a new destination
-// 
-// function createDestinationForm(){
-//     let destinationForm = document.getElementById("destination-form")
-// 
-//     destinationForm.innerHTML +=
-//     `
-//     <form>
-//         <label> Location: </label> <input type="text" id="name"><br>
-//         <label> Describe destination: </label> <input type="text" id="description"><br>
-//         <label> Add an image: </label> <input type="text" id="image_scr">
-//         <input type="submit" value="Create Destination">
-//     </form>
-//     `
-//     destinationForm.addEventListener("submit", destinationFormSubmission)
-// }
+//
 // 
 // // Submit new destination
 // 
@@ -108,31 +159,4 @@ function fetchDestination() {
 //         let d = new Destination(destination.id, destination.name, destination.description, destination.image_scr)
 //         d.renderDestination();
 //     })
-// }
-// 
-// // delete - delete a destination
-// 
-// function deleteDestination(){
-//     let destinationId = parseInt(event.target.dataset.id)
-// 
-//     fetch(`${BASE_URL}/destinations/${destinationId}`, {
-//         method: "DELETE"
-//     })
-// 
-//     this.location.reload()
-// }
-// 
-// // DELETE CATEGORY
-// 
-// function deleteCategory(){
-//     
-//     let categoryId = parseInt(event.target.dataset.id)
-//  
-//     fetch(`${BASE_URL}/categories/${categoryId}`, {
-//         method: 'DELETE'
-//     })
-//     // .then(resp => resp.json())
-//     // .then(() => e.target.parentNode.remove());
-// 
-//     // location.reload()
 // }
